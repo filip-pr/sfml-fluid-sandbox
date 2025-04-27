7b8de100a441b007067970071608ddd6a4b0a7ba
Filip Prášil <fi.prasil@gmail.com>
2025-04-27 16:08:11 +0200

    Add a close particle query to the spatial hash grid

--- src/spatial_hash_grid.cpp
+++ src/spatial_hash_grid.cpp
@@ -1,6 +1,8 @@
 
 #include "spatial_hash_grid.h"
 
+#include <cmath>
+
 void SpatialHashGrid::insert(Particle *particle)
 {
     size_t key = hash(particle->position);
@@ -20,4 +22,47 @@ void SpatialHashGrid::remove(Particle *particle)
         }
     }
     throw std::runtime_error(PARTICLE_NOT_FOUND_ERROR_MESSAGE);
+}
+
+inline std::vector<size_t> SpatialHashGrid::hash_cells(sf::Vector2f center, float radius) const
+{
+    std::vector<size_t> keys;
+
+    size_t min_cell_x = (center.x - radius) > 0 ? static_cast<size_t>(center.x - radius) / cell_size_ : 0;
+    size_t max_cell_x = static_cast<size_t>(center.x + radius) / cell_size_;
+    size_t min_cell_y = (center.y - radius) > 0 ? static_cast<size_t>(center.y - radius) / cell_size_ : 0;
+    size_t max_cell_y = static_cast<size_t>(center.y + radius) / cell_size_;
+
+    for (size_t x = min_cell_x; x <= max_cell_x; ++x)
+    {
+        for (size_t y = min_cell_y; y <= max_cell_y; ++y)
+        {
+            keys.push_back(hash_cell(x, y));
+        }
+    }
+    return keys;
+}
+
+std::vector<Particle *> SpatialHashGrid::query(sf::Vector2f center, float radius) const
+{
+    std::vector<Particle *> result;
+
+    auto keys = hash_cells(center, radius);
+    for (const auto &key : keys)
+    {
+        auto it = grid_.find(key);
+        if (it == grid_.end())
+            continue;
+
+        for (const auto &particle : it->second)
+        {
+            float distance = std::hypot(particle->position.x - center.x, particle->position.y - center.y);
+            if (distance <= radius)
+            {
+                result.push_back(particle);
+            }
+        }
+    }
+
+    return result;
 }
--- src/spatial_hash_grid.h
+++ src/spatial_hash_grid.h
@@ -3,6 +3,7 @@
 #define SPATIAL_HASH_GRID_H
 
 #include <unordered_map>
+#include <vector>
 #include <deque>
 
 #include "particle.h"
@@ -19,16 +20,26 @@ public:
     void insert(Particle *particle);
     void remove(Particle *particle);
 
+    std::vector<Particle *> query(sf::Vector2f center, float radius) const;
+
 private:
-    std::unordered_map<size_t, std::deque<Particle*>> grid_;
+    std::unordered_map<size_t, std::deque<Particle *>> grid_;
     size_t cell_size_;
 
-    size_t hash(sf::Vector2f position) const;
+    size_t hash(sf::Vector2f &position) const;
+
+    size_t hash_cell(size_t cell_x, size_t cell_y) const;
+    std::vector<size_t> hash_cells(sf::Vector2f center, float radius) const;
 };
 
-inline size_t SpatialHashGrid::hash(sf::Vector2f position) const
+inline size_t SpatialHashGrid::hash(sf::Vector2f &position) const
 {
-    return static_cast<size_t>(position.x) / cell_size_ + static_cast<size_t>(position.y) / cell_size_ * HASH_PRIME;
+    return static_cast<size_t>(position.x / cell_size_) + static_cast<size_t>(position.y / cell_size_) * HASH_PRIME;
+}
+
+inline size_t SpatialHashGrid::hash_cell(size_t cell_x, size_t cell_y) const
+{
+    return cell_x + cell_y * HASH_PRIME;
 }
 
 #endif
